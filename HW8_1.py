b7f51eea353b6eae0ec21f6c3de625ed46e20ca8
Marianne Gagnon <marianne.gagnon@temple.edu>
2017-04-23 21:23:20 -0400

    system call working

---
+++ HW8_1.py
@@ -0,0 +1,12 @@
+import os
+
+samples = []
+
+for file in os.listdir("/mnt/c/Users/SMG/Desktop/Yeast_replicates_files"):
+    if file.endswith(".fastq.gz"):
+        samples.append(file)
+
+for sample in samples:
+	command = "kallisto quant -i transcripts.idx -o output%s --single -l 180 -s 20 %s" % (samples.index(sample), sample)
+	print "Currently running: %s" % command
+	os.system(command) 
